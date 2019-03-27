12b6936a2b6ad57403e1dd8a6c3ddc3b5a0962a5
Matt Raines <mraines4@gmail.com>
2019-03-27 15:41:12 -0400

    adds quick test

--- test/tests.js
+++ test/tests.js
@@ -4,9 +4,19 @@ const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised).should();
 
+const ToDo = require('../models/todo');
+
 // describe('Sanity check', function() {
 //     it('should be 2', function() {
 //         // assert.equal(2, 1+1);
 //         expect(1+1).to.equal(2);
 //     });
 // });
+
+describe('todo model', () => {
+    it('should give todo by id', async () => {
+        const theTodo = await ToDo.getById(1);
+        console.log(theTodo)
+        expect(theTodo).to.be.an.instanceOf(ToDo);
+    })
+});
